be7deeaefe8aef6c72f343d7a461599a819d09c3
blandco <jbland@gmail.com>
2022-10-29 10:02:30 -0500

    completes V2

--- app/src/main/java/com/joelbland/hangmanv0/MainActivity.java
+++ app/src/main/java/com/joelbland/hangmanv0/MainActivity.java
@@ -1,7 +1,6 @@
 package com.joelbland.hangmanv0;
 
 import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -9,9 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-
 // V0 - Defining and Adding a Fragment to an Activity Using a Layout XML File
 // V1 - Adding GUI Components, Styles, Strings, and Colors
+// V2 - Defining a Fragment Using a Layout XML File and Adding the Fragment to an Activity by Code
+//  we display the fragment on the top right pane (with the blue background) of the screen showing
+//  the state of completion of the word, and an EditText for the user to enter a letter.
+//  We code the fragment in an XML file, fragment_game_state.xml, and create the fragment by code.
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +27,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView( R.layout.activity_main );
         TextView status = ( TextView ) findViewById( R.id.status );
         status.setText( "" + game.getGuessesLeft( ) );
+
+    /*FragmentManager fragmentManager = getFragmentManager( );
+    if( fragmentManager.findFragmentById( R.id.game_state ) == null ) {
+      FragmentTransaction transaction = fragmentManager.beginTransaction( );
+      GameStateFragment fragment = new GameStateFragment( );
+      transaction.add( R.id.game_state, fragment );
+      transaction.commit( );*/
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        GameStateFragment fragment = new GameStateFragment();
+        ft.add(R.id.game_state, fragment);
+        ft.commit();
+
+
+    }
+
+
+    public Hangman getGame( ) {
+        return game;
     }
 
     public void play( View view ) {
