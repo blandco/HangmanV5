322f5680c988c28b8dc3f79040f8068a1b440550
blandco <jbland@gmail.com>
2022-10-29 14:42:06 -0500

    completes V5

---
+++ app/src/main/java/com/joelbland/hangmanv0/BackgroundFragment.java
@@ -0,0 +1,13 @@
+package com.joelbland.hangmanv0;
+
+import androidx.fragment.app.Fragment;
+
+public class BackgroundFragment extends Fragment {
+
+    public BackgroundFragment( ) {
+    }
+
+    public String warning( ) {
+        return "ONLY 1 LEFT!";
+    }
+}
--- app/src/main/java/com/joelbland/hangmanv0/MainActivity.java
+++ app/src/main/java/com/joelbland/hangmanv0/MainActivity.java
@@ -1,11 +1,8 @@
 package com.joelbland.hangmanv0;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -13,92 +10,99 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-
-// V4 - Communication between Fragments and Their Activity: Enabling Play
-//  we complete the app by processing the user’s letter and enabling the user to play the game.
-//  We also illustrate how fragments can communicate with their activity when processing an event.
+// V5 - Using an Invisible Fragment
+//  We can also use a fragment that can perform some work in the background of the app,
+//  without any visual representation
 
 public class MainActivity extends AppCompatActivity {
 
     private Hangman game;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (game == null)
-            game = new Hangman(Hangman.DEFAULT_GUESSES);
-        setContentView(R.layout.activity_main);
-        TextView status = (TextView) findViewById(R.id.status);
-        status.setText("" + game.getGuessesLeft());
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        if ( game == null )
+            game = new Hangman( Hangman.DEFAULT_GUESSES );
+        setContentView( R.layout.activity_main );
+        TextView status = ( TextView ) findViewById( R.id.status );
+        status.setText( "" + game.getGuessesLeft( ) );
 
         //FragmentManager fragmentManager = getFragmentManager( );
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        if (fragmentManager.findFragmentById(R.id.game_state) == null) {
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            GameStateFragment fragment = new GameStateFragment();
-            transaction.add(R.id.game_state, fragment);
-            transaction.commit();
+        FragmentManager fragmentManager = getSupportFragmentManager( );
+
+        if( fragmentManager.findFragmentById( R.id.game_state ) == null ) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction( );
+            GameStateFragment fragment = new GameStateFragment( );
+            transaction.add( R.id.game_state, fragment );
+            transaction.commit( );
         }
 
-        if (fragmentManager.findFragmentById(R.id.game_result) == null) {
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            GameResultFragment fragment = new GameResultFragment();
-            transaction.add(R.id.game_result, fragment);
-            transaction.commit();
+        if( fragmentManager.findFragmentById( R.id.game_result ) == null ) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction( );
+            GameResultFragment fragment = new GameResultFragment( );
+            transaction.add( R.id.game_result, fragment );
+            transaction.commit( );
         }
 
+        if( fragmentManager.findFragmentByTag( "background" ) == null ) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction( );
+            BackgroundFragment fragment = new BackgroundFragment( );
+            transaction.add( fragment, "background" ); // tag is background
+            transaction.commit( );
+        }
     }
 
-    public Hangman getGame() {
+    public Hangman getGame( ) {
         return game;
     }
 
-    public void play(View view) {
-
-        EditText input = (EditText) findViewById(R.id.letter);
-
-
-
-        Editable userText = input.getText();
-        if (userText != null && userText.length() > 0) {
+    public void play( View view ) {
+        EditText input = (EditText) findViewById( R.id.letter );
+        Editable userText = input.getText( );
+        if( userText != null && userText.length( ) > 0 ) {
             // update number of guesses left
-            char letter = userText.charAt(0);
-            game.guess(letter);
-            TextView status = (TextView) findViewById(R.id.status);
-            status.setText("" + game.getGuessesLeft());
+            char letter = userText.charAt( 0 );
+            game.guess( letter );
+            TextView status = ( TextView ) findViewById( R.id.status );
+            status.setText( "" + game.getGuessesLeft() );
 
             // update incomplete word
-            //FragmentManager fragmentManager = getFragmentManager( );
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            GameStateFragment gsFragment = (GameStateFragment)
-                    fragmentManager.findFragmentById(R.id.game_state);
-            View gsFragmentView = gsFragment.getView();
-            TextView gameStateTV = (TextView)
-                    gsFragmentView.findViewById(R.id.state_of_game);
-            gameStateTV.setText(game.currentIncompleteWord());
+            // FragmentManager fragmentManager = getFragmentManager( );
+            FragmentManager fragmentManager = getSupportFragmentManager( );
+            GameStateFragment gsFragment = ( GameStateFragment )
+                    fragmentManager.findFragmentById( R.id.game_state );
+            View gsFragmentView = gsFragment.getView( );
+            TextView gameStateTV = ( TextView )
+                    gsFragmentView.findViewById( R.id.state_of_game );
+            gameStateTV.setText( game.currentIncompleteWord( ) );
 
             // clear EditText
-            input.setText("");
+            input.setText( "" );
 
-            int result = game.gameOver();
-            if (result != 0) /* game is over */ {
-                GameResultFragment grFragment = (GameResultFragment)
-                        fragmentManager.findFragmentById(R.id.game_result);
+            // check if there is only one guess left
+            if( game.getGuessesLeft( ) == 1 ) {
+                BackgroundFragment background = ( BackgroundFragment )
+                        fragmentManager.findFragmentByTag( "background" );
+                GameResultFragment grFragment = ( GameResultFragment )
+                        fragmentManager.findFragmentById( R.id.game_result );
+                // retrieve warning and display it
+                grFragment.setResult( background.warning( ) );
+            }
+
+            int result = game.gameOver( );
+            if( result != 0 ) /* game is over */ {
+                GameResultFragment grFragment = ( GameResultFragment )
+                        fragmentManager.findFragmentById( R.id.game_result );
 
                 // update TextView in result fragment
-                if (result == 1)
-                    grFragment.setResult("YOU WON");
-                else if (result == -1)
-                    grFragment.setResult("YOU LOST");
+                if( result == 1 )
+                    grFragment.setResult( "YOU WON" );
+                else if( result == -1 )
+                    grFragment.setResult( "YOU LOST" );
 
                 // delete hint in EditText
-                input.setHint("");
+                input.setHint( "" );
             }
         }
-    }
-
-    public static void hideKeyboardFrom(Context context, View view) {
-        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
