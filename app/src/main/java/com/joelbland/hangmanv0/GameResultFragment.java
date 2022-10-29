5fe0b48438470c4db9a8654bd1376161dff76adc
blandco <jbland@gmail.com>
2022-10-29 10:11:20 -0500

    completes V3

---
+++ app/src/main/java/com/joelbland/hangmanv0/GameResultFragment.java
@@ -0,0 +1,35 @@
+package com.joelbland.hangmanv0;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class GameResultFragment extends Fragment {
+    private TextView gameResultTV;
+
+    @Override
+    public View onCreateView( LayoutInflater inflater,
+                              ViewGroup container, Bundle savedInstanceState ) {
+        setUpFragmentGui( container );
+        return super.onCreateView( inflater, container,
+                savedInstanceState ) ;
+    }
+
+    public void setUpFragmentGui( ViewGroup container ) {
+        if( gameResultTV == null ) {
+            gameResultTV = new TextView( getActivity( ) );
+            gameResultTV.setGravity( Gravity.CENTER );
+            container.addView( gameResultTV );
+        }
+    }
+
+    public void onStart( ) {
+        super.onStart( );
+        gameResultTV.setText( "GOOD LUCK" );
+    }
+}
--- app/src/main/java/com/joelbland/hangmanv0/MainActivity.java
+++ app/src/main/java/com/joelbland/hangmanv0/MainActivity.java
@@ -8,12 +8,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+//import android.support.v7.app.AppCompatActivity;
+
 // V0 - Defining and Adding a Fragment to an Activity Using a Layout XML File
 // V1 - Adding GUI Components, Styles, Strings, and Colors
 // V2 - Defining a Fragment Using a Layout XML File and Adding the Fragment to an Activity by Code
 //  we display the fragment on the top right pane (with the blue background) of the screen showing
 //  the state of completion of the word, and an EditText for the user to enter a letter.
 //  We code the fragment in an XML file, fragment_game_state.xml, and create the fragment by code.
+// V3 -  Defining and Adding a Fragment to an Activity by Code
+//  we display the fragment on the bottom right pane (with the green background) of the screen
+//  showing a message about the result of the game in a TextView.
+//  This time, we do not use an XML file to define the fragment, we define and create it entirely by code.
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,22 +34,22 @@ public class MainActivity extends AppCompatActivity {
         TextView status = ( TextView ) findViewById( R.id.status );
         status.setText( "" + game.getGuessesLeft( ) );
 
-    /*FragmentManager fragmentManager = getFragmentManager( );
-    if( fragmentManager.findFragmentById( R.id.game_state ) == null ) {
-      FragmentTransaction transaction = fragmentManager.beginTransaction( );
-      GameStateFragment fragment = new GameStateFragment( );
-      transaction.add( R.id.game_state, fragment );
-      transaction.commit( );*/
+        FragmentManager fragmentManager = getSupportFragmentManager( );
+        if( fragmentManager.findFragmentById( R.id.game_state ) == null ) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction( );
+            GameStateFragment fragment = new GameStateFragment( );
+            transaction.add( R.id.game_state, fragment );
+            transaction.commit( );
+        }
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        GameStateFragment fragment = new GameStateFragment();
-        ft.add(R.id.game_state, fragment);
-        ft.commit();
-
+        if( fragmentManager.findFragmentById( R.id.game_result ) == null ) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction( );
+            GameResultFragment fragment = new GameResultFragment( );
+            transaction.add( R.id.game_result, fragment );
+            transaction.commit( );
+        }
 
     }
-
 
     public Hangman getGame( ) {
         return game;
